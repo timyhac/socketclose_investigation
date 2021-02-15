7b44b4fd837ae54f05ff6780c1fa5dd82b5ce7a0
timyhac <timyhac@gmail.com>
2021-02-15 18:29:17 +1100

    Added .NET Core example

---
+++ NetCore/Program.cs
@@ -0,0 +1,79 @@
+﻿using libplctag.NativeImport;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            try
+            {
+
+                var myLogger = new plctag.log_callback_func(MyLogger);
+                var statusAfterLoggerRegistration = plctag.plc_tag_register_logger(myLogger);
+
+                plctag.plc_tag_set_debug_level(2);
+
+                var tagHandle = plctag.plc_tag_create("protocol=ab_eip&gateway=192.168.0.10&path=1,0&plc=LGX&elem_size=4&elem_count=1&name=MY_DINT", 1000);
+
+                while (plctag.plc_tag_status(tagHandle) == 1)
+                {
+                    Thread.Sleep(100);
+                }
+                var statusBeforeRead = plctag.plc_tag_status(tagHandle);
+                if (statusBeforeRead != 0)
+                {
+                    Debug.WriteLine($"Something went wrong {statusBeforeRead}");
+                }
+
+
+                var myCallback = new plctag.callback_func(MyCallback);
+                var statusAfterRegistration = plctag.plc_tag_register_callback(tagHandle, myCallback);
+                if (statusAfterRegistration != 0)
+                {
+                    Debug.WriteLine($"Something went wrong {statusAfterRegistration}");
+                }
+
+                plctag.plc_tag_read(tagHandle, 1000);
+                while (plctag.plc_tag_status(tagHandle) == 1)
+                {
+                    Thread.Sleep(100);
+                }
+                var statusAfterRead = plctag.plc_tag_status(tagHandle);
+                if (statusAfterRead != 0)
+                {
+                    Debug.WriteLine($"Something went wrong {statusAfterRead}");
+                }
+
+                var value = (int)plctag.plc_tag_get_uint32(tagHandle, 0);
+                Console.WriteLine(value);
+
+                // Problem occurs here
+                var destroyResult = plctag.plc_tag_destroy(tagHandle);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void MyCallback(int tag_id, int event_id, int status)
+        {
+            Debug.WriteLine($"Tag Id: {tag_id}    Event Id: {event_id}    Status: {status}");
+        }
+
+        public static void MyLogger(int tag_id, int debug_level, string message)
+        {
+            Debug.Write($"Tag Id: {tag_id}    Debug Level: {debug_level}    Message: {message}");
+        }
+    }
+}
